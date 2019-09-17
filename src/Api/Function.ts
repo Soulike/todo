566a4fef75d863363e386755ba0db52d9d86e69d
Soulike <316413310@qq.com>
2019-09-17 12:51:17 +0800

    删除 electron

--- src/Api/Function.ts
+++ src/Api/Function.ts
@@ -1,12 +1,4 @@
 export function prefix(url: string): string
 {
-    const {NODE_ENV} = process.env;
-    if (NODE_ENV !== 'production')
-    {
-        return `/server${url}`;
-    }
-    else
-    {
-        return `https://todo.soulike.tech/server${url}`;
-    }
+    return `/server${url}`;
 }
--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import {BrowserRouter, MemoryRouter, Redirect, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
 import {PAGE_ID, PAGE_ID_TO_COMPONENT, PAGE_ID_TO_ROUTE} from '../CONFIG/PAGE';
 import NotFound from '../Component/NotFound';
 
-const route = (
-    <Switch>
-        <Redirect path={'/'} to={PAGE_ID_TO_ROUTE[PAGE_ID.LOGIN]} exact={true} />
-        {
-            Object.values(PAGE_ID).map(pageId => <Route key={pageId}
-                                                        exact={true}
-                                                        path={PAGE_ID_TO_ROUTE[pageId]}
-                                                        component={PAGE_ID_TO_COMPONENT[pageId]} />)
-        }
-        <Route render={NotFound} />
-    </Switch>
-);
-
 export default () =>
 {
-    return process.env.NODE_ENV === 'production' ?
-        (<MemoryRouter>{route}</MemoryRouter>) :
-        (<BrowserRouter>{route}</BrowserRouter>);
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Redirect path={'/'} to={PAGE_ID_TO_ROUTE[PAGE_ID.LOGIN]} exact={true} />
+                {
+                    Object.values(PAGE_ID).map(pageId => <Route key={pageId}
+                                                                exact={true}
+                                                                path={PAGE_ID_TO_ROUTE[pageId]}
+                                                                component={PAGE_ID_TO_COMPONENT[pageId]} />)
+                }
+                <Route render={NotFound} />
+            </Switch>
+        </BrowserRouter>
+    );
 };
